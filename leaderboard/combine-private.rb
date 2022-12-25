656f9b7de91493c75cf6fa09ff1c78ece5c79329
Peter Tseng <pht24@cornell.edu>
2022-12-25 09:45:26 +0000

    combine-private: Add -s option to filter by star count

--- leaderboard/combine-private.rb
+++ leaderboard/combine-private.rb
@@ -1,5 +1,11 @@
 require_relative 'private'
 require 'json'
+require 'optparse'
+
+OptionParser.new { |opts|
+  opts.banner = "Usage: #$PROGRAM_NAME [options]"
+  opts.on('-s STARS', '--stars', Integer, 'required number of stars')
+}.parse!(into: opts = {})
 
 jsons = ARGV.map { |a| JSON.parse(File.read(a)) }
 
@@ -7,4 +13,7 @@ years = jsons.map { |j| Integer(j['event']) }
 raise "mismatching years #{years}" if years.uniq.size > 1
 
 people = jsons.flat_map { |j| j['members'].values }
+if required_stars = opts[:stars]
+  people.select! { |p| p['stars'] >= required_stars }
+end
 recalculate_private(people, year: years[0])
